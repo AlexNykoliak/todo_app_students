e5e684e18fd31fc1fd28bad5ed9dfac702a58162
AlexNykoliak <nykolyak.o@gmail.com>
2023-12-08 17:36:08 +0200

    upd

--- todo_frontend/src/components/UserLogin.vue
+++ todo_frontend/src/components/UserLogin.vue
@@ -11,6 +11,7 @@
   
   <script>
   export default {
+    name: 'UserLogin',
     data() {
       return {
         username: '',
--- todo_frontend/src/router/index.js
+++ todo_frontend/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Login from '../components/Login.vue';
+import Login from '../components/UserLogin.vue';
 import TaskList from '../components/TaskList.vue';
 
 Vue.use(Router);
